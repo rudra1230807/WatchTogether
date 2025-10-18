6230de7a165cc7b0dc56c6e10fd7991405841132
RUDRAPRATAP ANGAD PAGHAL <rudra@RUDRAPRATAPs-MacBook-Air.local>
2025-10-18 16:24:27 +0530

    Add start.sh and requirements.txt with proper dependencies

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Ensure pip is updated
-python3 -m pip install --upgrade pip
-
-# Install dependencies
-pip install -r requirements.txt
-
 # Start FastAPI in background
 uvicorn server:app --host 0.0.0.0 --port 8000 &
 
